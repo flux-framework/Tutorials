bbebd5b3201fce8cfc0667803e514c484eb549fc
Stephen Herbein <stephen272@gmail.com>
2020-01-27 19:03:31 -0800

    example2: switch to python3 and fix broken she-bang line

--- 2020-ECP/workflow-examples/example2/compute.py
+++ 2020-ECP/workflow-examples/example2/compute.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 import time
@@ -13,5 +13,5 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-print "Will compute for " + str(args.integer) + " seconds."
+print("Will compute for " + str(args.integer) + " seconds.")
 time.sleep(args.integer)
