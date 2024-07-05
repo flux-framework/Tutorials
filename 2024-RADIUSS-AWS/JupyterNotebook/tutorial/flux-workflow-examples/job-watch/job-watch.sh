95798a0891a737f76929c954dc15671ae59aa61c
vsoch <vsoch@users.noreply.github.com>
2024-07-04 22:49:35 -0600

    refactor: content and organization for RADIUSS 2024
    I am starting preliminary changes for RADIUSS 2024. So far:
    - the launcher is fixed to load the tutorials via buttons
    - I am moving "core" flux tutorial content separate from
      what we can consider external or plugins, like dyad. The
      reason is because these are experimental and change from
      year to year, and having it alongside core content makes
      a promise about consistency that I am not sure we can
      keep. I have not done this yet, but I am going to separate
      the files (images and notebooks) cleanly as well.
    - top level: everything is there for the tutorial user when
      the notebook opens, no need to navigate into many sub-
      directories.
    - terminal button: instead of "click this long list of
      annoying paths to open a terminal" I figured out how to
      make a button in the notebook directly.
    - marginal content changes: I am starting to tweak / update
      content, this will be a multi-step process.
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- 2024-RADIUSS-AWS/JupyterNotebook/tutorial/flux-workflow-examples/job-watch/job-watch.sh
+++ 2024-RADIUSS-AWS/JupyterNotebook/tutorial/flux-workflow-examples/job-watch/job-watch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "25 blueberry pancakes on the table... 25 blueberry pancakes! 🥞️"
+echo "25 chocolate chip pancakes on the table... 25 chocolate chip pancakes! 🥞️"
 sleep 3
-echo "Eat a stack, for a snack, 15 blueberry pancakes on the table! 🥄️"
+echo "Eat a stack, for a snack, 15 chocolate chip pancakes on the table! 🥄️"
 sleep 3
-echo "15 blueberry pancakes on the table... 15 blueberry pancakes! 🥞️"
+echo "15 chocolate chip pancakes on the table... 15 chocolate chip pancakes! 🥞️"
 sleep 2
-echo "Throw a stack... it makes a smack! 15 blueberry pancakes on the wall! 🥞️"
+echo "Throw a stack... it makes a smack! 15 chocolate chip pancakes on the wall! 🥞️"
 sleep 2
 echo "You got some cleaning to do 🧽️"
